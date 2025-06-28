fe13dcd81b332a27c5bfb4257943056a7e1bccb8
KannadaCinemaStore <kannadacinemastore@gmail.com>
2025-06-28 21:14:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 
 # ForceSub Channel & Log Channels
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002245219361'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', ''))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002245219361'))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001913629397'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '')) 
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', ''))
@@ -43,15 +43,15 @@ FILES_DATABASE = environ.get('FILES_DATABASE', "mongodb+srv://King2:King2@cluste
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ravikumar1cn')
 
 # Other Channel's
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', ''))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002245219361'))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
-request_channel = environ.get('REQUEST_CHANNEL', '') 
+request_channel = environ.get('REQUEST_CHANNEL', '-1002245219361') 
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '')) 
 
 # Added Link Here Not Id 
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', '')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1002245219361')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/KannadaMagaaZone')
 
 # Verification
 IS_VERIFY = is_enabled('IS_VERIFY', False)
